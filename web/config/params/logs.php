1ede4b1d7d610e5e61fc17e1fc254d76c4fb8b2b
RasulArslanov <rasularslanoov@gmail.com>
2020-08-04 13:11:05 +0500

    Fixed logs url

--- web/config/params/logs.php
+++ web/config/params/logs.php
@@ -2,6 +2,6 @@
 
 return [
     'logs' => [
-        'url' => 'http://' .  file_get_contents(__DIR__ . '/../../server.ip') . ':57152/logs.php?key=' . file_get_contents(__DIR__ . '/../../.key')
+        'url' => 'http://' .  trim(file_get_contents(__DIR__ . '/../../server.ip')) . ':57152/logs.php?key=' . file_get_contents(__DIR__ . '/../../.key')
     ]
 ];
